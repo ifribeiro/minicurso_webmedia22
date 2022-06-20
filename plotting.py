e0994e1873ef077c93af91636d7bf37e48e3cfbe
Iran Ribeiro <ifreitasribeiro@hotmail.com>
2022-06-19 23:58:22 -0300

    new plot functions

--- plotting.py
+++ plotting.py
@@ -1,5 +1,7 @@
+from cProfile import label
 import datetime
 import pandas as pd
+import numpy as np
 import matplotlib.pyplot as plt
 
 def get_list_dates(data_size=None, year=None, month=None, day=1, tdeltah=1):
@@ -53,7 +55,7 @@ def add_week_hr(df, timesteps=24):
 
     return df
 
-def get_df(list_dates=None, real_data=None, timesteps=24):
+def get_df(list_dates=None, data=None, timesteps=24):
     """
     Returns a pandas dataframe the columns to be used in the visualizations
 
@@ -64,14 +66,13 @@ def get_df(list_dates=None, real_data=None, timesteps=24):
     """
     
     dict_real = {'date':list_dates}
-    for i in range(real_data.shape[1]):
-        dict_real['cnt_{}'.format(i)] = real_data[:,i]
+    for i in range(data.shape[1]):
+        dict_real['cnt_{}'.format(i)] = data[:,i]
 
     df_real = pd.DataFrame(dict_real)
     df_real = add_week_hr(df_real)
     ts = [t for t in range(timesteps)]
-    df_real['ts'] = ts * (real_data.shape[0]//timesteps)
-
+    df_real['ts'] = ts * (data.shape[0]//timesteps)
     return df_real
 
 def plot_sum_real(list_cnt_real = [], fake_data = None, list_dates=None, figtitle="",
@@ -92,5 +93,36 @@ def plot_sum_real(list_cnt_real = [], fake_data = None, list_dates=None, figtitl
     plt.clf()
     plt.close()
 
-def plot_compare_sum():
-    pass
+def get_list_wks(model_samples, real_data, list_dates_real, timestep):
+    """    
+    params:
+
+    - real data (for shape)
+    - list dates
+    - wk
+    """
+    list_wks = []
+    for i, s in enumerate(model_samples):
+        # reshape
+        s = np.load(s)
+        s = s.reshape(len(list_dates_real), 1)
+        # s = s[:3287]
+        # s = np.exp(s)
+        # s = s[:, :, idx]
+        #sample_reshaped = s.reshape(250, 48, 1)
+        df_sample = get_df(list_dates_real, s, timesteps=timestep)    
+        wk_fake = get_count(df_sample,4,timestep=timestep, column='ts')
+        list_wks.append(wk_fake['cnt_0'])
+        if (i % 2 == 0):
+            print ("Done {}!".format(i))
+    return list_wks
+
+def plot_compare_sum(real, somas, std):
+    fig, ax = plt.subplots()
+    x = np.arange(0,24,1)
+    ax.plot(x, real, label='real', marker='.',ls='--')
+    ax.plot(x, somas)
+    ax.fill_between(x, somas-std, somas+std, alpha=0.3)
+    ax.set_xlabel("Hora",fontdict={'fontsize':14})
+    ax.set_ylabel("Bicicletas alugadas",fontdict={'fontsize':14})
+    plt.legend()
