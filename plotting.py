2e4925a36eed860ccdac6f73025512d59ff313d3
Iran Ribeiro <ifreitasribeiro@hotmail.com>
2022-11-03 20:34:49 -0300

    parteI

--- plotting.py
+++ plotting.py
@@ -1,5 +1,4 @@
 import datetime
-from pickle import FALSE
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
@@ -187,3 +186,34 @@ def plot_compare_sum(dict_weeks, dict_weeks_real, bbox, figtitle="", scaler=None
     plt.legend(lista_handles, wks_name, bbox_to_anchor=bbox, loc='lower right')
     plt.suptitle("{}".format(figtitle))
     return fig, axes
+
+###########
+# Minicurso
+###########
+
+def plot_series(time=[], series=[], format="-", markers=[None,"o"], start=0, end=None, label=[], figsize=(10,5)):
+    fig, ax = plt.subplots(figsize=figsize)
+    for t, s, m, l in zip(time, series, markers, label):
+        ax.plot(t[start:end], s[start:end], format, label=l, marker=m)
+    ax.set_xlabel("Tempo")
+    ax.set_ylabel("Valor")
+    if len(label)>1:
+        plt.legend(fontsize=14)
+
+def trend(time, slope=0):
+    return slope * time
+
+def white_noise(time, noise_level=1, seed=None):
+    rnd = np.random.RandomState(seed)
+    return rnd.randn(len(time)) * noise_level
+
+def seasonal_pattern(season_time):
+    """Just an arbitrary pattern, you can change it if you wish"""
+    return np.where(season_time < 0.4,
+                    np.cos(season_time * 2 * np.pi),
+                    1 / np.exp(3 * season_time))
+
+def seasonality(time, period, amplitude=1, phase=0):
+    """Repeats the same pattern at each period"""
+    season_time = ((time + phase) % period) / period
+    return amplitude * seasonal_pattern(season_time)
